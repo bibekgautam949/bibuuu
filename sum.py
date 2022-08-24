883bc0ed517717e2d6f9f1f0997af2dce61490b4
bibek ncit <bibek.171544@ncit.edu.np>
2022-08-24 16:32:51 +0545

    hehe

--- sum.py
+++ sum.py
@@ -6,3 +6,4 @@ num1 ,num2 = input('Enter two numbers  separated by space  ').split()
 print('The numbers are:',num1, num2)
 print('Addition is:')
 print('Sum of numbers is ', int(num1)+ int(num2))
+ 
