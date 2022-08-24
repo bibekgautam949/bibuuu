a82773b9a2b56ff23c7e11c51efb768f0eb71168
bibek ncit <bibek.171544@ncit.edu.np>
2022-08-24 16:08:02 +0545

    Added FizFuzz

---
+++ BoomBoom.py
@@ -0,0 +1,9 @@
+for numbers in range(1,100):
+    if numbers % 3 == 0 and numbers % 5 == 0:
+        print('Boom Boom ',numbers)
+    elif numbers % 3 == 0:
+        print('Boom ',numbers)
+    elif numbers % 5 == 0:
+        print('Nepse ',numbers)
+    else:
+        print('Doom ',numbers)
