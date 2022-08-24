85b6e6ce0771ac86a52529eda209e9e89ef32ff7
bibek ncit <bibek.171544@ncit.edu.np>
2022-08-24 16:11:23 +0545

    Modified Committed

--- sum.py
+++ sum.py
@@ -1,4 +1,8 @@
 
+from cgi import print_arguments
+
+
 num1 ,num2 = input('Enter two numbers  separated by space  ').split()
 print('The numbers are:',num1, num2)
+print('Addition is:')
 print('Sum of numbers is ', int(num1)+ int(num2))
